3f5eb76c227f8f788e45034ff7b5a8c8d9561fc4
unknown <dlehddnr444@naver.com>
2019-05-04 20:03:31 +0900

    dhodksehl

--- js/sessionCheck.js
+++ js/sessionCheck.js
@@ -1,12 +1,11 @@
     //유저가 로그인 했는지 안했는지 확인해주는 함수
     function userSessionCheck() {
-        console.log(currentTime);
         //로그인이 되어있으면 - 유저가 있으면, user를 인자값으로 넘겨준다.
-        firebaseEmailAuth.onAuthStateChanged(function (user) {
+        firebase.auth().onAuthStateChanged(function (user) {
           
           if (user) {
             //조회 - 데이터 베이스에 저장된 닉네임을 현재 접속되어 있는 user의 pk key 값을 이용해서 가져오기
-            firebaseDatabase.ref("users/" + user.uid).once('value').then(function (snapshot) {
+              database.ref("users/" + user.uid).once('value').then(function (snapshot) {
               
               //자바스크립트 dom 선택자를 통해서 네비게이션 메뉴의 엘리먼트 변경해주기
                 document.getElementById("signoutfors").textContent = "로그아웃";
